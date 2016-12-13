42b4f6729a8bb166c401172a57e632733bb55431
Rory O'Connor <7roconnor@stpatrick.org>
2016-12-13 13:54:22 -0600

    Add files via upload

---
+++ password.js
@@ -0,0 +1,15 @@
+class Password{
+  //The first part of the class.
+  constructor(PubKey, PriKey)
+  {
+    this.PublicKey = PubKey; //String
+    this.PrivateKey = PriKey; //String of numbers "xxxx-xxxx-xxxx"
+  }
+
+
+  //Instance functions below this comment.
+
+
+
+  //Static function below this comment.
+}
